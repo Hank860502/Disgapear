a32f986e2f2d6a09177fddd821c8a3f059d60478
Hank860502 <hank860502@gmail.com>
2016-10-27 12:08:53 +0800

    change home page from html.erb to html.haml

--- disgapear/app/controllers/sessions_controller.rb
+++ disgapear/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to home_index_path
+      redirect_to disgapear_index_path
     else
       flash[:danger] = "Invalid combination"
       redirect_to '/login'
