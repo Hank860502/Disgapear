456b148c21d4cda71444eb4e5268d756bacf4b93
Hank860502 <hank860502@gmail.com>
2016-10-27 23:15:08 +0800

    make new form for creat essay

--- disgapear/app/controllers/essays_controller.rb
+++ disgapear/app/controllers/essays_controller.rb
@@ -3,9 +3,35 @@ class EssaysController < ApplicationController
 	end
 
 	def new
+		@essay = Essay.new
 	end
+
+	def create
+    @essay = Essay.new(essay_params)
+    respond_to do |format|
+      if @essay.save
+        session[:essay_id] = @essay.id
+        format.html { redirect_to root_path, notice: 'Essay was successfully created.' }
+        format.json { render :show, status: :created, location: @essay }
+        # redirect_to root_path
+      else
+        flash[:danger] = @essay.errors.full_messages.to_sentence
+        format.html { render :new }
+        format.json { render json: @essay.errors, status: :unprocessable_entity }
+
+        # render 'new'
+      end
+    end
+  end
 
 	def show
 		@essay= Essay.find(params[:id])
 	end
+
+	private
+
+	def essay_params
+    params.require(:essay).permit(:topic, :description, :content,:word_limit)
+  end
+
 end
