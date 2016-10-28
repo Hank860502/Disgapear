af34808c886b5eb82975d2984533020c7f7a9299
Hank860502 <hank860502@gmail.com>
2016-10-28 11:00:13 +0800

    user is able to send in essay, fix issue for nil user_id when creating new essay

--- disgapear/app/controllers/essays_controller.rb
+++ disgapear/app/controllers/essays_controller.rb
@@ -4,17 +4,22 @@ class EssaysController < ApplicationController
 
 	def new
 		@essay = Essay.new
-    @user = current_user
 	end
 
 	def create
     @essay = Essay.new(essay_params)
-
+    @essay.user_id = current_user.id
+    p '================'
+    p @essay
+    p '================'
     respond_to do |format|
       if @essay.save
         # session[:essay_id] = @essay.id
         format.html { redirect_to essays_path, notice: 'Essay was successfully created.' }
         format.json { render :show, status: :created, location: @essay }
+        p '++++++++++++'
+        p @essay
+        p '++++++++++++'
         # redirect_to root_path
       else
         flash[:danger] = @essay.errors.full_messages.to_sentence
@@ -33,7 +38,7 @@ class EssaysController < ApplicationController
 	private
 
 	def essay_params
-    params.require(:essay).permit(:topic, :description, :content,:word_limit)
+    params.require(:essay).permit(:user_id, :topic, :description, :content,:word_limit)
   end
 
 end
